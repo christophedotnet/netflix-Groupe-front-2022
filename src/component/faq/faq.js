d08330debf036b0cc33985370c0c22269116853b
gaetan <gs99@hotmail.fr>
2022-07-11 16:13:13 +0200

    rework faq

--- src/component/faq/faq.js
+++ src/component/faq/faq.js
@@ -1,46 +1,35 @@
-import './faq.css';
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import './faq.css'
 
 function Faq() {
+
+  let faqs = useSelector(state=>state.faqs)
+
   return (
+    <div className="backgroundBlack">
     <div className="faq m-auto">
-      <div className="accordion" id="accordionExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingOne">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-              Accordion Item #1
-            </button>
-          </h2>
-          <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      <div className="accordion accordion-flush" id="accordionExample">
+      {
+        faqs.map((e,i)=>{
+          return(
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button className="accordion-button collapsed faq" type="button" data-bs-toggle="collapse" data-bs-target={"#collapseOne"+i} aria-expanded="false" aria-controls={"collapseOne"+i}>
+                  { e.Question }
+                </button>
+              </h2>
+              <div id={"collapseOne"+i} className="accordion-collapse collapse faq1" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  { e.Response }
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Accordion Item #2
-            </button>
-          </h2>
-          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingThree">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              Accordion Item #3
-            </button>
-          </h2>
-          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
+          )
+        })
+      }
       </div>
+    </div>
     </div>
   );
 }
--- src/redux/netflix/netflixReducer.js
+++ src/redux/netflix/netflixReducer.js
@@ -1,20 +1,48 @@
 const INITIAL_STATE = {
     user : null,
+    faqs: [
+        {
+            Question:"Question 1",
+            Response: "Response 1"
+        },
+        {
+            Question:"Question 2",
+            Response: "Response 2"
+        },
+        {
+            Question:"Question 3",
+            Response: "Response 3"
+        },
+        {
+            Question:"Question 4",
+            Response: "Response 4"
+        },
+        {
+            Question:"Question 5",
+            Response: "Response 5"
+        }
+    ],
     ressources : []
 }
 
 function NetflixReducer (state = INITIAL_STATE, action) {
     switch (action.type) {
-        case 'GET-RESSOURCE': {
+        case 'ADD-RESSOURCE': {
             return{
                 ...state,
                 ressources: [...state.ressources, action.payload]
             }
         }
+        case 'ADD-FAQ': {
+            return{
+                ...state,
+                faqs: [...state.faqs, action.payload]
+            }
+        }
         case 'SET-USER': {
             return{
                 ...state,
-                listePokemonsCaptures: [...state.listePokemonsCaptures, action.payload]
+                user: action.payload
             }
         }
         default: return state

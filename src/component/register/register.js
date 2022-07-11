67a7b9a9b008b8962d9cb421bbd88f97b7a8df9a
gaetan <gs99@hotmail.fr>
2022-07-11 15:44:42 +0200

    register modif

--- src/component/register/register.js
+++ src/component/register/register.js
@@ -1,28 +1,42 @@
-import './register.css';
 import Navbar from "./../navbar/navbar"
+import { Link } from 'react-router-dom'
+import './register.css'
 
 function Register() {
     return(
         <>
-            <Navbar id={0}></Navbar>
-            <div className="signin m-auto">
-            <form>
-                <div className="mb-3 test">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+      <Navbar/>
+        <div className="signin m-auto">
+          <form>
+          <h4 class="mb-3">Register</h4>
+            <div className="mb-3 test">
+              <label className="form-label">First name</label>
+              <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" />
             </div>
+            <div className="mb-3 test">
+              <label className="form-label">Last name</label>
+              <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3 test">
+              <label className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3 test">
+              <label className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword" />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword" />
+            </div>
+            <div className="mb-3">
+              <button type="submit" className="btn btn-danger">Submit</button>
+            </div>
+            <div className="mb-3">
+              If already have a account, <Link to="/login">click here</Link>
+            </div>
+          </form>
+        </div>
     </>
     )
 }
